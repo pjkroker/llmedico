16348cd09b18761d201897522cf1287376abfc80
Paul Jakob Kroker <pauljakobkroker@gmail.com>
2025-11-16 18:40:56 +0100

    fixed bug

--- se_helpers/src/se_helpers/files/files.py
+++ se_helpers/src/se_helpers/files/files.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 def save_json_to_file(json_string: str, out_path: str | Path):
@@ -5,3 +6,13 @@ def save_json_to_file(json_string: str, out_path: str | Path):
     out_path.parent.mkdir(parents=True, exist_ok=True)
     with out_path.open("w", encoding="utf8") as f:
         f.write(json_string)
+
+def load_json(file_path: Path) -> dict:
+    # Open the file and load its contents
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    # Check type
+    # if isinstance(data, list) and len(data) == 1:
+    #     data = data[0] #convert from array to dict
+    return data
