30d5962113294081f790d1a6be7e2bb438499d32
Paul Jakob Kroker <pauljakobkroker@gmail.com>
2025-11-20 18:08:31 +0100

    added cli

--- llmedico/src/llmedico/commands/analyze.py
+++ llmedico/src/llmedico/commands/analyze.py
@@ -1,8 +1,45 @@
 import click
+from pathlib import Path
+from llmedico.main import main as run_pipeline
 
 @click.command(name="analyze")
-@click.argument("path")
-@click.option("--method", help="Method name to analyze")
-def analyze_command(path, method):
-    """Analyze a JSON file."""
-    click.echo(f"Analyzing {path}, method={method}")
+@click.option(
+    "--target-class",
+    "fq_class_name",
+    required=True,
+    help="Fully qualified class name, e.g. 'org.apache.commons.lang3.StringUtils'",
+)
+@click.option(
+    "--target-method",
+    "target_method",
+    default="",
+    help="Target method to analyze (optional)",
+)
+@click.option(
+    "--data-dir",
+    "path_data_dir",
+    type=click.Path(exists=True, file_okay=False, path_type=Path),
+    required=True,
+    help="Directory containing input/output data",
+)
+@click.option(
+    "--out-dir",
+    "path_output_dir",
+    type=click.Path(file_okay=False, path_type=Path),
+    required=True,
+    help="Output directory for generated artifacts",
+)
+
+def analyze_command(fq_class_name, target_method, path_data_dir, path_output_dir):
+    """
+    Analyze a Java class and generate method assertions / data.
+    """
+
+    click.echo("=== LLMedico: analyze ===")
+    click.echo(f"Target class:   {fq_class_name}")
+    click.echo(f"Target method:  {target_method}")
+    click.echo(f"Data dir:       {path_data_dir}")
+    click.echo(f"Output dir:     {path_output_dir}")
+
+    run_pipeline(fq_class_name, target_method, path_data_dir, path_output_dir)
+
--- llmedico/src/llmedico/main.py
+++ llmedico/src/llmedico/main.py
@@ -9,95 +9,143 @@ import logging
 from pyrandoop.pyrandoop import PyRandoop
 from se_helpers.files.files import save_json_to_file, load_json, save_realy_json_to_file
 
-# Set up basic configuration for logging
-logging.basicConfig(
-    filename='/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/output/' + 'log.txt',
-    level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    force=True
-)
-logging.debug("---Starting LLMedico---")
-logging.debug("---Starting JDoctor - Extracting JavaDoc---")
-FQ_CLASS_NAME = "org.apache.commons.lang3.StringUtils"
+
+
+# PATH_CLASS_DIR = "" #--class-dir #TODO ignore for now
 IMAGE = "pjkroker/toradocu-x86-extractor"
-pyjdoctor = PyJDoctor("/Users/paul/paul_data/projects_cs/ba_versuch1/pyjdoctor", IMAGE)
-pyjdoctor.set_data_dir("/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data")
-pyjdoctor.set_output_dir("/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/output")
-pyjdoctor.set_input_dir("Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/input")
+LIMIT_METHODS = 40 # defines the number of methods of the class to be analyzed #TODO change
+def start_jdoctor(path_data_dir: Path, path_output_dir: Path, fq_class_name: str) -> None:
+    pyjdoctor = PyJDoctor("/Users/paul/paul_data/projects_cs/ba_versuch1/pyjdoctor", IMAGE)
+    pyjdoctor.set_data_dir(path_data_dir)
+    pyjdoctor.set_output_dir(path_output_dir)
 
-pyjdoctor.start_container()
-pyjdoctor.generate_all(FQ_CLASS_NAME)
-pyjdoctor.stop_container()
+    pyjdoctor.start_container()
+    pyjdoctor.generate_all(fq_class_name)
+    pyjdoctor.stop_container()
 
-logging.debug("---Starting JavaParser - Extracting JavaDoc---")
-jp = JavaParser()
-result_json = jp.extract_to_json("/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/input/repository/src/main/java/org/apache/commons/lang3/StringUtils.java")
-save_json_to_file(result_json,"/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/output/result.json")
-result_json = json.loads(result_json)
+def start_java_parser(path_output_dir: Path, path_java_class: Path):
+    jp = JavaParser()
+    result_json = jp.extract_to_json(path_java_class)
+    save_json_to_file(result_json, path_output_dir / "result.json")
+    result_json = json.loads(result_json)
+    return result_json
 
-logging.debug("---Starting Translator - Translating JavaDoc to Assertions---")
-trans = Translator()
-LIMIT = 3
-results = {}
+def start_translator(result_json):
+    trans = Translator()
+    results = {}
 
-for i in range(0, LIMIT):
-    method_name = result_json[0]["methods"][i]["name"]
-    javadoc = result_json[0]["methods"][i]["javadoc"]
-    javacode = result_json[0]["methods"][i]["code"]
-    java_assertions = trans.translate_javadoc(javacode)
-    logging.debug(java_assertions)
-    results[method_name] = java_assertions
-logging.debug(results)
+    for i in range(0, LIMIT_METHODS):
+        method_name = result_json[0]["methods"][i]["name"]
+        javadoc = result_json[0]["methods"][i]["javadoc"]
+        javacode = result_json[0]["methods"][i]["code"]
+        java_assertions = trans.translate_javadoc(javacode)
+        logging.debug(java_assertions)
+        results[method_name] = java_assertions
+    logging.debug(results)
+    return results
 
+def start_translator_with_specified_method(result_json, target_method:str):
+    trans = Translator()
+    results = {}
+    print(target_method)
 
-logging.debug("---Validating Syntax of generated Assertions---")
-valid = {}
-for method_name in results:
-    valid[method_name] = []
-    for assertion in results[method_name]:
-        if jp.is_valid_java_assert(assertion):
-            valid[method_name].append(assertion)
-        else:
-            logging.debug(f"{assertion} is not a valid java assertion")
-
-logging.debug(valid)
-
-logging.debug("---Adding valid Assertions to Randoop File???---")
-#load jdoctor file, remove everything except the one method
-#overwrite pre, post and throws with generated assertions
-jdoctor = load_json(Path("/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/output/toradocu-randoop_specs.json"))
-specs = []
-for method_name in valid:
-    try:
-        one_method = Translator.copy_entry_by_method(jdoctor, method_name)
-        pre_conditions = []
-        for assertion in valid[method_name]:
-            pre_condition = Translator.assertion_to_json(assertion)
-            logging.debug(pre_condition)
-            pre_conditions.append(pre_condition)
-        one_method["pre"] = pre_conditions
-        specs.append(one_method)
-    except ValueError:
-        logging.warning(f"No entry found for method name '{method_name}'")
+    for i in range(0, LIMIT_METHODS):
+        method_name = result_json[0]["methods"][i]["name"]
+        if method_name == target_method:
+            javadoc = result_json[0]["methods"][i]["javadoc"]
+            javacode = result_json[0]["methods"][i]["code"]
+            java_assertions = trans.translate_javadoc(javacode)
+            logging.debug(java_assertions)
+            results[method_name] = java_assertions
+    logging.debug(results)
+    return results
 
 
-logging.debug(specs)
-save_realy_json_to_file(specs, "/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/output/" + "specs.json")
+def start_validating(results):
+    jp = JavaParser()
+    valid = {}
+    for method_name in results:
+        valid[method_name] = []
+        for assertion in results[method_name]:
+            if jp.is_valid_java_assert(assertion):
+                valid[method_name].append(assertion)
+            else:
+                logging.debug(f"{assertion} is not a valid java assertion")
 
-rd = PyRandoop(path_output_dir="/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/output",
-               class_dir="/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/input/repository/target/classes",
-               time_limit=60, deterministic=False)
-result = rd.generate_dependencies(
-    path_class_file="/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/input/repository/target/classes/org/apache/commons/lang3/StringUtils.class")
+    logging.debug(valid)
+    return valid
 
-result = rd.generate_error_revealing_tests(fq_class_name="org.apache.commons.lang3.StringUtils", path_oracles=Path(
-    "/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/output/specs.json"))
-logging.debug(result["stdout"])
+def start_building_randoop_file(valid, path_output_dir: Path):
+    # load jdoctor file, remove everything except the one method
+    # overwrite pre, post and throws with generated assertions
+    jdoctor = load_json( path_output_dir / "toradocu-randoop_specs.json")
+    specs = []
+    for method_name in valid:
+        try:
+            one_method = Translator.copy_entry_by_method(jdoctor, method_name)
+            pre_conditions = []
+            for assertion in valid[method_name]:
+                pre_condition = Translator.assertion_to_json(assertion)
+                logging.debug(pre_condition)
+                pre_conditions.append(pre_condition)
+            one_method["pre"] = pre_conditions
+            specs.append(one_method)
+        except ValueError:
+            logging.warning(f"No entry found for method name '{method_name}'")
+
+    logging.debug(specs)
+    save_realy_json_to_file(specs, path_output_dir / "llmedico-specs.json")
+
+def start_randoop(path_output_dir: Path, fq_class_name: str) -> None:
+    rd = PyRandoop(path_output_dir=path_output_dir,
+                   class_dir="/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/input/repository/target/classes",
+                   time_limit=60, deterministic=False)
+    result = rd.generate_dependencies(
+        path_class_file="/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/input/repository/target/classes/org/apache/commons/lang3/StringUtils.class")
+
+    result = rd.generate_error_revealing_tests(fq_class_name=fq_class_name, path_oracles=(path_output_dir / "llmedico-specs.json"))
+    logging.debug(result["stdout"])
+
+def main(fq_class_name: str, target_method: str, path_data_dir: Path, path_output_dir: Path):
+    relative_path = fq_class_name.replace(".", "/") + ".java"
+    path_java_class = path_data_dir / "input" / "repository" / "src" / "main" / "java" / relative_path
+    # Set up basic configuration for logging
+    logging.basicConfig(
+        filename=path_output_dir / 'log.txt',
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        force=True
+    )
+    logging.debug("---Starting LLMedico---")
+    logging.debug("---Starting JDoctor - Extracting JavaDoc---")
+    start_jdoctor(path_data_dir, path_output_dir, fq_class_name)
+
+    logging.debug("---Starting JavaParser - Extracting JavaDoc---")
+    result_json = start_java_parser(path_output_dir, path_java_class)
+
+    logging.debug("---Starting Translator - Translating JavaDoc to Assertions---")
+    results = start_translator_with_specified_method(result_json, target_method)
+
+    logging.debug("---Validating Syntax of generated Assertions---")
+    valid = start_validating(results)
+
+    logging.debug("---Adding valid Assertions to Randoop File???---")
+    start_building_randoop_file(valid, path_output_dir)
+
+    logging.debug("---Generating Tests with Randoop File---")
+    start_randoop(path_output_dir, fq_class_name)
+    logging.debug("---Ending LLMedico---")
 
 
+if __name__ == '__main__':
+    FQ_CLASS_NAME = "org.apache.commons.lang3.StringUtils"  # --target-class java class to be analyzed
+    TARGET_METHOD = "isNotEmpty"  # --target-method#
+    PATH_DATA_DIR = Path(
+        "/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data")  # --data-dir #TODO change it so that is just project/src/..
+    PATH_OUTPUT_DIR = Path(
+        "/Users/paul/paul_data/projects_cs/ba_versuch1/llmedico/data/output")  # --out-dir #TODO copy if different
 
-
-
-
-
-
+    main(fq_class_name=FQ_CLASS_NAME,
+         target_method=TARGET_METHOD,
+         path_data_dir=PATH_DATA_DIR,
+         path_output_dir=PATH_OUTPUT_DIR,)
