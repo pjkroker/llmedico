d76f7b03e6e89eb1272d09f823096b521ec9d0cd
Paul Jakob Kroker <pauljakobkroker@gmail.com>
2025-12-09 14:01:56 +0100

    added dockerfile with version 3.0 (JDoctor) of Toradocu, instead of pulling latest from github

--- pyjdoctor/scripts/setup.sh
+++ pyjdoctor/scripts/setup.sh
@@ -9,4 +9,4 @@ echo "Building Docker Container"
 pwd
 #--no-cache 
 #--build-arg CACHEBUST=$(date +%s)
-docker build --build-arg CACHEBUST=$(date +%s) -f ./dockerfile --platform=linux/amd64 -t pjkroker/toradocu-x86-extractor .
+docker build --build-arg CACHEBUST=$(date +%s) -f ./dockerfile-jdoctor --platform=linux/amd64 -t pjkroker/toradocu-3.0-x86 .
