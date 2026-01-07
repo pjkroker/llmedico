f424c6aa538e4ebabeca1bfabbaadabe0cff87c5
Paul Jakob Kroker <pauljakobkroker@gmail.com>
2026-01-07 18:52:52 +0100

    added evaluation script

--- llmedico/scripts/run_evaluation.py
+++ llmedico/scripts/run_evaluation.py
@@ -1,13 +1,7 @@
-import json
 import os
 from pathlib import Path
 
-import llmedico
-from llmedico.builder.class_model_builder import ClassModelBuilder
-from llmedico.builder.class_model_builder_jdoctor import ClassModelBuilderJdoctor
-from llmedico.evaluation.evaluation_csv_writer import EvaluationCSVWriter
-from llmedico.evaluation.evaluator import evaluate_class
-from llmedico.main import main
+from llmedico.evaluation.evaluation import evaluate, InputFormat
 
 path = Path(__file__).parent.parent / "storage" / "jgrapht-core-0.9.2.txt"
 
@@ -15,7 +9,7 @@ with path.open("r", encoding="utf-8") as f:
     lines = [line.strip() for line in f if line.strip()]
 
 path_goals = Path(__file__).parent.parent / "storage" / "goal-output-groundtruth" / "jgrapht-core-0.9.2"
-path_llmedico = Path(__file__).parent / "results" / "jgrapht-core-0.9.2"
+path_llmedico = Path(__file__).parent / "results-translation" / "jgrapht-core-0.9.2"
 
 
 print(len(lines))
@@ -28,21 +22,8 @@ for fqn in lines:
         if not path_out_dir.exists():
             os.makedirs(path_out_dir)
 
-
-        builder = ClassModelBuilder()
-        input_path = path_llmedico / fqn / "llmedico-condition_translator.json"
-        with open(input_path, "r", encoding="utf-8") as f:
-            extracted_conditions = json.load(f)
-        generated_cls = builder.build_class(extracted_conditions[0])
-
-        builder = ClassModelBuilderJdoctor()
-        input_path = path_goals / (fqn + "_goal.json")
-        with open(input_path, "r", encoding="utf-8") as f:
-            extracted_conditions = json.load(f)
-        expected_cls = builder.build_class(extracted_conditions)
-        result = evaluate_class(expected_cls, generated_cls)
-
-        path_outputfile = base / f"result_{fqn}.csv"
-        with EvaluationCSVWriter(path_outputfile) as writer:
-            for row in result:
-                writer.write(row)
+        evaluate(path_goals / (fqn + "_goal.json"),
+                 InputFormat.JDOCTOR,
+                 path_llmedico / fqn / "llmedico-condition_translator.json",
+                 InputFormat.LLMEDICO,
+                 path_out_dir)
