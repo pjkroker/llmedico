58c52299f0b51b1bf3279174f4f1c35ce823cc77
Paul Jakob Kroker <pauljakobkroker@gmail.com>
2026-01-14 15:58:59 +0100

    added write by type

--- llmedico/scripts/run_evaluation.py
+++ llmedico/scripts/run_evaluation.py
@@ -2,8 +2,9 @@ import os
 import datetime
 from pathlib import Path
 
-from llmedico.evaluation.evaluation import evaluate, InputFormat
+from llmedico.evaluation.evaluation import evaluate, InputFormat, _write_evaluation_rows_by_type
 from llmedico.evaluation.evaluator import evaluate_class
+from llmedico.evaluation.result import AssertionRelation
 from llmedico.evaluation_metrics.evaluation_metrics_writer import EvaluationMetricsCSVWriter
 from llmedico.evaluation_metrics.evaluator_metrics import _compute_metrics_class, MetricMode, \
     compute_metrics_all_classes, compute_metrics_project
@@ -58,76 +59,47 @@ projects = {"commons-collections4-4.1":
 path_ground_truth_base = Path(__file__).parent.parent / "storage" / "goal-output-groundtruth"
 path_llmedico_base = path_llmedico = Path(__file__).parent / "results-translation"
 path_jdoctor_base = Path(__file__).parent / "results-translation-jdoctor"
+path_out_base_jdoctor = Path(__file__).parent / "results-evalutation-jdoctor"
 path_out_base = Path(__file__).parent / "results-evalutation"
 date_eval = "-2026-01-11"
 #TODO fix these for comparison
 done_jdoocc = [ "commons-math3-3.6.1", "freecol-0.11.6", "guava-19.0","plume-lib-1.1.0" ] #["commons-collections4-4.1", "commons-math3-3.6.1", "freecol-0.11.6", "gs-core-1.3", "guava-19.0", "plume-lib-1.1.0"]
-done = ["commons-math3-3.6.1", "freecol-0.11.6", "gs-core-1.3", "guava-19.0", "jgrapht-core-0.9.2", "plume-lib-1.1.0"]
+done = []#["commons-collections4-4.1", "commons-math3-3.6.1", "freecol-0.11.6", "guava-19.0", "jgrapht-core-0.9.2", "plume-lib-1.1.0"]
 date = datetime.date.today()
 evaluation_rows = []
 for project in projects:
     print(project)
     evaluation_rows_project = []
-    path_outputfile = path_out_base / (project + date_eval)  # TODO
+    path_outputfile = path_out_base_jdoctor / (project + date_eval)  # TODO
     if project not in done:
         path_classes = Path(__file__).parent.parent / "storage" / "projects_txt" / (project + ".txt")
         with path_classes.open("r", encoding="utf-8") as f:
             classes_ = [line.strip() for line in f if line.strip()]
         print(classes_)
         for class_ in classes_:
-            evaluation_rows_project.append(evaluate( path_jdoctor_base / (project + date_eval) / class_ / "toradocu-condition_translator.json", #path_ground_truth_base / project / (class_ + "_goal.json"),
+            evaluation_rows_project.append(evaluate( path_ground_truth_base / project / (class_ + "_goal.json"),#path_jdoctor_base / (project + date_eval) / class_ / "toradocu-condition_translator.json",
                                             InputFormat.JDOCTOR,
-                                            path_llmedico_base / (project + date_eval) / class_ / "llmedico-condition_translator.json",
-                                            InputFormat.LLMEDICO,
+                                            path_jdoctor_base / (project + date_eval) / class_ / "toradocu-condition_translator.json", #path_jdoctor_base / (project + date_eval) / class_ / "toradocu-condition_translator.json"
+                                            InputFormat.JDOCTOR,
                                             path_outputfile))
 
-        compute_metrics_all_classes(evaluation_rows_project, project, path_outputfile, MetricMode("relaxed"))
-
-        compute_metrics_project(evaluation_rows_project, project, path_outputfile, MetricMode("relaxed"))
-
+        compute_metrics_all_classes(evaluation_rows_project, project, path_outputfile, MetricMode("jdoctor"))
+        #TODO count all classes
+        compute_metrics_project(evaluation_rows_project, project, path_outputfile, MetricMode("jdoctor"))
+        #TODO count project
         evaluation_rows.append([row for rows_class in evaluation_rows_project for row in rows_class])
 
-compute_metrics_project(evaluation_rows, "all_projects", path_out_base, MetricMode("relaxed"))
+compute_metrics_project(evaluation_rows, "all_projects", path_out_base_jdoctor, MetricMode("jdoctor"))
+types = [AssertionRelation("stronger"), AssertionRelation("weaker"),
+         AssertionRelation("unsupported"), AssertionRelation("equivalent"),
+         AssertionRelation("incomparable"), AssertionRelation("dual") ]
+flat_rows = [row for sublist in evaluation_rows for row in sublist]
+_write_evaluation_rows_by_type(flat_rows, types,path_out_base_jdoctor / "rows_by_type.csv")
+from collections import Counter
+relation_counts = Counter(row.relation for row in flat_rows)
 
-# # for every class
-# path_outputfile = Path(__file__).parent / "results-evalutation" / "commons-math3-3.6.1-2026-01-10"
-# compute_metrics_all_classes(evaluation_rows, "org.jgrapht", path_outputfile, MetricMode("llmedico"))
-# #for project
-# compute_metrics_project(evaluation_rows, "org.jgrapht", path_outputfile, MetricMode("llmedico"))
-#
+print(relation_counts)
 
-# path = Path(__file__).parent.parent / "storage" / "projects_txt" / "commons-math3-3.6.1.txt"
-#
-#
-# with path.open("r", encoding="utf-8") as f:
-#     lines = [line.strip() for line in f if line.strip()]
-#
-# path_goals = Path(__file__).parent.parent / "storage" / "goal-output-groundtruth" / "commons-math3-3.6.1"
-# path_llmedico = Path(__file__).parent / "results-translation" / "commons-math3-3.6.1-2026-01-10"
-#
-#
-# print(len(lines))
-# print(lines)
-# done = []
-# evaluation_rows = []
-# for fqn in lines:
-#     if fqn not in done:
-#         base = Path(__file__).parent / "results-evalutation" / "commons-math3-3.6.1-2026-01-10"
-#         path_out_dir = base
-#         if not path_out_dir.exists():
-#             os.makedirs(path_out_dir)
-#
-#         evaluation_rows.append(evaluate(path_goals / (fqn + "_goal.json"),
-#                  InputFormat.JDOCTOR,
-#                  path_llmedico / fqn / "llmedico-condition_translator.json",
-#                  InputFormat.LLMEDICO,
-#                  path_out_dir))
-
-# for every class
-# path_outputfile = Path(__file__).parent / "results-evalutation" / "commons-math3-3.6.1-2026-01-10"
-# compute_metrics_all_classes(evaluation_rows, "org.jgrapht", path_outputfile, MetricMode("llmedico"))
-# #for project
-# compute_metrics_project(evaluation_rows, "org.jgrapht", path_outputfile, MetricMode("llmedico"))
 
 
 
