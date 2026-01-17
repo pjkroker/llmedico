7dc88b3d7cfe4723bba06df30712abec63f981e9
Paul Jakob Kroker <pauljakobkroker@gmail.com>
2026-01-17 12:55:37 +0100

    added first_aid_repair

--- llmedico/src/llmedico/translator/translation_repair_loop.py
+++ llmedico/src/llmedico/translator/translation_repair_loop.py
@@ -1,4 +1,8 @@
 import logging
+from typing import Optional
+
+from llm_caller.utils.processing import extract_code_by_language
+
 logger = logging.getLogger(__name__)
 
 class TranslationRepairLoop:
@@ -7,7 +11,20 @@ class TranslationRepairLoop:
         self.validator = validator
         self.max_iters = max_iters
 
-    def translate_with_repair(self, javadoc: str, method_name: str, parameters: list[str], return_type:str,method_selection:str, mode: str, errors, expected_len: int, previous_output: str) -> str:
+    def _first_aid_repair(self, raw_response: str, errors: list[str], expected_len: int) -> Optional[str]:
+        if errors[0].startswith("No code snippets found for language"):
+            logger.debug("Trying to fix output format")
+            broken_response = extract_code_by_language(raw_response, "")
+            fixed_response = "```json\n" + broken_response[0] + "\n```"
+
+            errors = self.validator.validate(fixed_response, expected_len)
+            if errors:
+                self._first_aid_repair(fixed_response, errors, expected_len)
+            else: return fixed_response
+        logger.debug("Could not manually fix llm response!")
+        return None
+
+    def translate_with_repair(self, javadoc: str, method_name: str, parameters: list[str], return_type:str, method_selection:str, mode: str, errors: list[str], expected_len: int, previous_output: str) -> str:
         logger.debug('Trying to fix initial solution with repair loop.')
         i = 0
         result = ""
@@ -22,5 +39,11 @@ class TranslationRepairLoop:
             logger.info(f"Repair successful, after {i} out of a maximum of {self.max_iters} iterations.")
             return result
         else:
+            logger.debug("Repair loop failed. Try to fix it manually.")
+            repair = self._first_aid_repair(result, errors, expected_len)
+            if repair:
+                logger.warning(f"Could fix manually but repair loop failed, after {i} out of a maximum of {self.max_iters} iterations.")
+                return repair
+
             logger.warning(f"Repair failed, after {i} out of a maximum of {self.max_iters} iterations.")
             return "```json\n[]\n```"
---
+++ llmedico/tests/test_translator/test_repair_loop.py
@@ -0,0 +1,52 @@
+from llm_caller.models.ollama import Ollama
+from llmedico.translator.condition_validator import ConditionValidator
+from llmedico.translator.translation_repair_loop import TranslationRepairLoop
+from llmedico.translator.translator import Translator
+
+
+def test_first_aid():
+    raw_output = """
+    Here is the corrected output:
+
+    ```
+    [{
+        "description": "Returns a synchronized (thread-safe) bag backed by the given bag.",
+        "assertion": "assert !methodResultID.isInstanceOf(Bag.class);",
+        "name": null,
+        "content": "Returns a synchronized (thread-safe) bag backed by the given bag."
+    }]
+    ```"""
+    errors = ['No code snippets found for language json. \nMake sure the code blocks are wrapped exactly like shown in the provided example!\nInclude the code inbetween ```json and ```']
+    validator = ConditionValidator("json")
+    trans = Translator(Ollama("hallo"))
+    repair_loop = TranslationRepairLoop(trans, validator)
+    fixed = repair_loop._first_aid_repair(raw_response=raw_output, errors=errors, expected_len=1)
+
+    corrected_string = """```json
+[{
+        "description": "Returns a synchronized (thread-safe) bag backed by the given bag.",
+        "assertion": "assert !methodResultID.isInstanceOf(Bag.class);",
+        "name": null,
+        "content": "Returns a synchronized (thread-safe) bag backed by the given bag."
+    }]
+```"""
+
+    assert fixed == corrected_string
+
+    fixed = repair_loop._first_aid_repair(raw_response=raw_output, errors=["dummy"], expected_len=1)
+    assert fixed is None
+
+    raw_output_two_erros = """
+        Here is the corrected output:
+
+        ```
+        [{
+            "description": "Returns a synchronized (thread-safe) bag backed by the given bag.",
+            "assertion": "assert !methodResultID.isInstanceOf(Bag.class)",
+            "name": null,
+            "content": "Returns a synchronized (thread-safe) bag backed by the given bag."
+        }]
+        ```"""
+    errors = ['No code snippets found for language json.']
+    fixed_two_erros = repair_loop._first_aid_repair(raw_response=raw_output_two_erros, errors=errors, expected_len=1)
+    assert fixed_two_erros is None
