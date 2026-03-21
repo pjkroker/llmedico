e6a33988b111806073ae821a50c8a3bc68cfdd22
Paul Jakob Kroker <pauljakobkroker@gmail.com>
2026-03-21 15:45:40 +0100

    added tests

--- llmedico/tests/test_evaluation_z3/test_evaluator_z3.py
+++ llmedico/tests/test_evaluation_z3/test_evaluator_z3.py
@@ -1,8 +1,11 @@
+from llmedico.translator import translator
 from llmedico.z3_evaluation.ast_normalizer import AstNormalizer
+from llmedico.z3_evaluation.model_ast import Conditional, Compare, Var, IntConst, InstanceOf, Method
 from llmedico.z3_evaluation.preprocessing import normalize_expression, tokenize, rewrite_method_references
 from llmedico.evaluation.result import AssertionRelation
 from llmedico.z3_evaluation.string_parser import StringParser
 from llmedico.z3_evaluation.z3_evaluator import AssertionEvaluatorZ
+from llmedico.z3_evaluation.z3_translator import Z3Translator
 
 
 def test_evaluator_equal():
@@ -308,3 +311,157 @@ def test2():
     result = evaluator.evaluate(expected_ast, generated_ast)
 
     assert result.relation == AssertionRelation.EQUIVALENT
+
+def test_evaluation_pipeline_steps():
+    assert1 = "foo(y)"
+    expr = rewrite_method_references(normalize_expression(assert1))
+    tokens = tokenize(expr)
+    parser = StringParser(tokens)
+    expected_ast = parser.parse()
+    assert expected_ast == Method(receiver=None, name='foo', parameters=[Var(name='y')])
+
+
+    assert1 = "x.foo(y)"
+    expr = rewrite_method_references(normalize_expression(assert1))
+    tokens = tokenize(expr)
+    parser = StringParser(tokens)
+    expected_ast = parser.parse()
+    assert expected_ast == Method(receiver=Var(name='x'), name='foo', parameters=[Var(name='y')])
+
+    assert1 = "x > y"
+    expr = rewrite_method_references(normalize_expression(assert1))
+    tokens = tokenize(expr)
+    parser = StringParser(tokens)
+    expected_ast = parser.parse()
+    assert expected_ast == Compare(left=Var(name='x'), op='>', right=Var(name='y'))
+
+    assert1 = "X instanceof String"
+    expr = rewrite_method_references(normalize_expression(assert1))
+    tokens = tokenize(expr)
+    parser = StringParser(tokens)
+    expected_ast = parser.parse()
+    assert expected_ast == InstanceOf(expr=Var(name='X'), type_name='String')
+
+    assert1 = "x>0?x:y"
+    expr = rewrite_method_references(normalize_expression(assert1))
+    tokens = tokenize(expr)
+    parser = StringParser(tokens)
+    expected_ast = parser.parse()
+    assert expected_ast == Conditional(cond=Compare(left=Var(name='x'), op='>', right=IntConst(value=0)), then=Var(name='x'), otherwise=Var(name='y'))
+
+    assert1 = "java.util.Arrays.stream(args[0]).anyMatch(e -> e==null)"
+    norm = AstNormalizer()
+    expected_tokens = tokenize(rewrite_method_references(normalize_expression(assert1)))
+    parser = StringParser(tokens=expected_tokens, normalise_incomplete_java=True)
+    expected_ast = parser.parse()
+    expected_ast = norm.normalize_expr(expected_ast)
+
+    translator = Z3Translator()
+    expected_logic = translator.translate(expected_ast, None)
+
+
+    assert2 = "stream(args[0]).anyMatch(e -> e==null)"
+    norm = AstNormalizer()
+    expected_tokens = tokenize(rewrite_method_references(normalize_expression(assert2)))
+    parser = StringParser(tokens=expected_tokens, normalise_incomplete_java=True)
+    expected_ast2 = parser.parse()
+    expected_ast2 = norm.normalize_expr(expected_ast2)
+
+
+    translator = Z3Translator()
+    expected_logic2 = translator.translate(expected_ast2, None)
+
+    evaluator = AssertionEvaluatorZ()
+
+
+    result = evaluator.evaluate(expected_ast, expected_ast2, None)
+
+    assert expected_logic == expected_logic2
+    assert result.relation == AssertionRelation.EQUIVALENT
+
+    assert_anyMatch = "args[0].stream().anyMatch(e -> e==null)"
+    norm = AstNormalizer()
+    expected_tokens = tokenize(rewrite_method_references(normalize_expression(assert_anyMatch)))
+    parser = StringParser(tokens=expected_tokens, normalise_incomplete_java=True)
+    expected_ast = parser.parse()
+    expected_ast = norm.normalize_expr(expected_ast)
+    print(expected_ast)
+    translator = Z3Translator()
+    expected_logic = translator.translate(expected_ast, None)
+
+    assert_allMatch = "args[0].stream().allMatch(e -> e==null)"
+    norm = AstNormalizer()
+    expected_tokens = tokenize(rewrite_method_references(normalize_expression(assert_allMatch)))
+    parser = StringParser(tokens=expected_tokens, normalise_incomplete_java=True)
+    expected_ast = parser.parse()
+    expected_ast = norm.normalize_expr(expected_ast)
+    print(expected_ast)
+    translator = Z3Translator()
+    expected_logic = translator.translate(expected_ast, None)
+
+    assert_noneMatch = "args[0].stream().noneMatch(e -> e==null)"
+    norm = AstNormalizer()
+    expected_tokens = tokenize(rewrite_method_references(normalize_expression(assert_noneMatch)))
+    parser = StringParser(tokens=expected_tokens, normalise_incomplete_java=True)
+    expected_ast = parser.parse()
+    expected_ast = norm.normalize_expr(expected_ast)
+    print(expected_ast)
+    translator = Z3Translator()
+    expected_logic = translator.translate(expected_ast, None)
+
+    assert_no_stream = "args[0].noneMatch(e -> e==null)"
+    norm = AstNormalizer()
+    expected_tokens = tokenize(rewrite_method_references(normalize_expression(assert_no_stream)))
+    parser = StringParser(tokens=expected_tokens, normalise_incomplete_java=True)
+    expected_ast = parser.parse()
+    expected_ast = norm.normalize_expr(expected_ast)
+    print(expected_ast)
+    translator = Z3Translator()
+    expected_logic = translator.translate(expected_ast, None)
+
+
+def test_typing_paper():
+    assert_no_stream = " x >> y"
+    norm = AstNormalizer()
+    expected_tokens = tokenize(rewrite_method_references(normalize_expression(assert_no_stream)))
+    parser = StringParser(tokens=expected_tokens, normalise_incomplete_java=True)
+    expected_ast = parser.parse()
+    expected_ast = norm.normalize_expr(expected_ast)
+    print(expected_ast)
+    translator = Z3Translator()
+    expected_logic = translator.translate(expected_ast, None)
+
+    assert_no_stream = "true?x"
+    norm = AstNormalizer()
+    expected_tokens = tokenize(rewrite_method_references(normalize_expression(assert_no_stream)))
+    parser = StringParser(tokens=expected_tokens, normalise_incomplete_java=True)
+    expected_ast = parser.parse()
+    expected_ast = norm.normalize_expr(expected_ast)
+    print(expected_ast)
+    translator = Z3Translator()
+    expected_logic = translator.translate(expected_ast, None)
+
+    assert_no_stream = "x instanceof pkg1.pgk2.String"
+    norm = AstNormalizer()
+    expected_tokens = tokenize(rewrite_method_references(normalize_expression(assert_no_stream)))
+    parser = StringParser(tokens=expected_tokens, normalise_incomplete_java=True)
+    expected_ast = parser.parse()
+    expected_ast = norm.normalize_expr(expected_ast)
+    print(expected_ast)
+    translator = Z3Translator()
+    expected_logic = translator.translate(expected_ast, None)
+
+    assert_no_stream = " x == 1 &&  x > 0"
+    norm = AstNormalizer()
+    expected_tokens = tokenize(rewrite_method_references(normalize_expression(assert_no_stream)))
+    parser = StringParser(tokens=expected_tokens, normalise_incomplete_java=True)
+    expected_ast = parser.parse()
+    expected_ast = norm.normalize_expr(expected_ast)
+    print(expected_ast)
+    translator = Z3Translator()
+    expected_logic = translator.translate(expected_ast, None)
+    print(expected_logic)
+
+
+
+
